384c5315bb992c74d2d482cdeca3cc62ccf562c4
Juan Ortiz <jportiz32@gmail.com>
2019-05-24 02:23:28 -0400

    Initial commit

---
+++ src/test/java/com/badwitsoftware/dictionary/DictionaryTest.java
@@ -0,0 +1,36 @@
+package com.badwitsoftware.dictionary;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import com.badwitsoftware.dictionary.Dictionary;
+
+public class DictionaryTest {
+
+	@Test
+	void testBerries() {
+		Dictionary dictionary = new Dictionary(
+				Arrays.asList("cherry", "pineapple", "melon", "strawberry", "raspberry"));
+		assertEquals("strawberry", dictionary.findMostSimilar("strawbery"));
+		assertEquals("cherry", dictionary.findMostSimilar("berry"));
+	}
+
+	@Test
+	void testLanguages() {
+		Dictionary dictionary = new Dictionary(
+				Arrays.asList("javascript", "java", "ruby", "php", "python", "coffeescript"));
+		assertEquals("java", dictionary.findMostSimilar("heaven"));
+		assertEquals("javascript", dictionary.findMostSimilar("javascript"));
+	}
+	
+	@Test
+	void testCities() {
+		Dictionary dictionary = new Dictionary(Arrays.asList("dubai", "dublin", "new york", "new jersey"));
+		assertEquals("dubai", dictionary.findMostSimilar("dublai"));
+		assertEquals("new york", dictionary.findMostSimilar("new yarks"));
+	}
+
+}
