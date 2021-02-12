86938903f25ced824271df15cf7d5e0f61bf79d6
Juan Ortiz <notjuanortiz@gmail.com>
2021-02-12 12:58:30 -0500

    Move subproject to root directory
    Also:
     * Fixed issue with JUnit5 not playing nicely with Gradle
     * Include stanford-nlp dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     `java-library`
+    idea
+    eclipse
 }
+
+group = "com.wordturtle"
+version = "0.0.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -11,5 +16,10 @@ repositories {
 }
 
 dependencies {
+    implementation("edu.stanford.nlp:stanford-corenlp:4.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
